f2b82d4aca87fdf456b734d58f725e1d299f7818
Snævar Geirsson <snaevar@snaev.ar>
2023-06-09 03:07:55 +0000

    Added a toggle to change the language.

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Tailwind CSS is used to make it look nice.
 Enter the amount of flour to be used. Other ingredient amounts are automatically calculated based on configurable percentages for each. 
 
 The type of yeast to be used can be configured as well.
+
+A toggle in the bottom right corner allows switching between English and Icelandic.
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 'use client'
 import { useState, useEffect } from "react";
+import "node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function BreadPage() {
   const initialFlour = 500;
@@ -17,6 +18,8 @@ export default function BreadPage() {
   const [saltPercentage, setSaltPercentage] = useState(initialSaltPercentage);
   const [yeastPercentage, setYeastPercentage] = useState(initialYeastPercentage);
   const [yeastType, setYeastType] = useState("dry");
+
+  const [language, setLanguage] = useState("english");
 
   useEffect(() => {
     setWater(flour * (hydration / 100));
@@ -55,12 +58,34 @@ export default function BreadPage() {
     }
   };
 
+  const handleLanguageSwitch = () => {
+    const newLanguage = language === "english" ? "icelandic" : "english";
+    setLanguage(newLanguage);
+  };
+
+  const titleText = language === "english" ? "Bread Dough Calculator" : "Reiknivél Fyrir Brauðdeig"
+  const gramsText = language === "english" ? "grams" : "grömm";
+  const flourText = language === "english" ? "Flour" : "Hveiti";
+  const waterText = language === "english" ? "Water" : "Vatn";
+  const optionsText = language === "english" ? "Options" : "Valmöguleikar";
+  const hydrationLabelText = language === "english" ? "Hydration" : "Vatnshlutfall";
+  const saltPercentageText = language === "english" ? "Salt Percentage" : "Salthlutfall";
+  const yeastPercentageText = language === "english" ? "Yeast Percentage" : "Gerhlutfall";
+  const sourdoughPercentageText = language === "english" ? "Sourdough Percentage" : "Súrdeigshlutfall";
+  const saltLabelText = language === "english" ? "Salt" : "Salt";
+  const yeastLabelText = language === "english" ? "Yeast" : "Ger";
+  const yeastTypeText = language === "english" ? "Yeast Type" : "Gertegund";
+  const dryYeastText = language === "english" ? "Dry Yeast" : "Þurrger";
+  const freshYeastText = language === "english" ? "Fresh Yeast" : "Pressuger";
+  const sourdoughText = language === "english" ? "Sourdough" : "Súrdeig";
+  const resetText = language === "english" ? "Reset" : "Endurræsa";
+
   return (
     <div className="w-full max-w-lg">
-      <div className="min-w-max bg-white shadow-md rounded m-10 p-6">
-        <h1 className="block text-gray-700 text-md font-semibold mb-2">Bread Dough Calculator</h1>
+      <div className="min-w-max bg-white shadow-md rounded m-5 p-5">
+        <h1 className="block text-gray-700 text-md font-semibold mb-2">{titleText}</h1>
         <div className="mb-4 input-container">
-          <label className="block text-gray-700 text-sm font-semibold mb-2">Flour</label>
+          <label className="block text-gray-700 text-sm font-semibold mb-2">{flourText}</label>
           <div className="relative w-28">
             <input
               type="number"
@@ -69,12 +94,12 @@ export default function BreadPage() {
               onChange={(e) => setFlour(Number(e.target.value))}
             />
             <span className="absolute bottom-3 end-2 text-gray-500 text-xs">
-              grams
+            {gramsText}
             </span>
           </div>
         </div>
         <div className="mb-4 input-container">
-          <label className="block text-gray-700 text-sm font-semibold mb-2">Water</label>
+          <label className="block text-gray-700 text-sm font-semibold mb-2">{waterText}</label>
           <div className="relative w-28">
             <input
               type="number"
@@ -84,12 +109,12 @@ export default function BreadPage() {
               readOnly
             />
             <span className="absolute bottom-3 end-2 text-gray-500 text-xs">
-              grams
+            {gramsText}
             </span>
         </div>
         </div>
         <div className="mb-4 input-container">
-          <label className="block text-gray-700 text-sm font-semibold mb-2">Salt</label>
+          <label className="block text-gray-700 text-sm font-semibold mb-2">{saltLabelText}</label>
           <div className="relative w-28">
             <input
               type="number"
@@ -99,12 +124,12 @@ export default function BreadPage() {
               readOnly
             />
             <span className="absolute bottom-3 end-2 text-gray-500 text-xs">
-              grams
+            {gramsText}
             </span>
           </div>
         </div>
         <div className="mb-4 input-container">
-          <label className="block text-gray-700 text-sm font-semibold mb-2">Yeast</label>
+          <label className="block text-gray-700 text-sm font-semibold mb-2">{yeastLabelText}</label>
           <div className="relative w-28">
             <input
               type="number"
@@ -114,14 +139,14 @@ export default function BreadPage() {
               readOnly
             />
             <span className="absolute bottom-3 end-2 text-gray-500 text-xs">
-              grams
+            {gramsText}
             </span>
           </div>
         </div>
         <div className="mb-4">
-        <h1 className="block text-gray-700 text-md font-semibold mb-2">Options</h1>
+        <h1 className="block text-gray-700 text-md font-semibold mb-2">{optionsText}</h1>
           <div className="mb-2">
-            <label className="block mb-1">Hydration: {hydration}%</label>
+            <label className="block mb-1">{hydrationLabelText}: {hydration}%</label>
             <input
               type="range"
               min={40}
@@ -132,7 +157,7 @@ export default function BreadPage() {
             />
           </div>
           <div className="mb-2">
-            <label className="block mb-1">Salt Percentage: {saltPercentage}%</label>
+            <label className="block mb-1">{saltPercentageText}: {saltPercentage}%</label>
             <input
               type="range"
               min={1.5}
@@ -146,14 +171,14 @@ export default function BreadPage() {
           <div className="mb-2">
             <label className="block mb-1">
               {yeastType === "dry"
-                ? `Yeast Percentage: ${yeastPercentage}%`
+                ? `${yeastPercentageText}: ${yeastPercentage}%`
                 : yeastType === "fresh"
-                ? `Yeast Percentage: ${yeastPercentage}%`
-                : `Starter Percentage: ${yeastPercentage}%`}
+                ? `${yeastPercentageText}: ${yeastPercentage}%`
+                : `${sourdoughPercentageText}: ${yeastPercentage}%`}
             </label>
             <input
               type="range"
-              min={yeastType === "dry" ? 0.3 : yeastType === "fresh" ? 0.7 : 10}
+              min={yeastType === "dry" ? 0.3 : yeastType === "fresh" ? 0.7 : 2}
               max={yeastType === "dry" ? 2.5 : yeastType === "fresh" ? 5 : 50}
               step={yeastType === "dry" ? 0.1 : yeastType === "fresh" ? 0.1 : 1}
               value={yeastPercentage}
@@ -162,7 +187,7 @@ export default function BreadPage() {
             />
           </div>
           <div className="mb-2">
-            <label className="block mb-1">Yeast Type:</label>
+            <label className="block mb-1">{yeastTypeText}:</label>
             <label className="block mb-1">
               <input
                 type="checkbox"
@@ -170,7 +195,7 @@ export default function BreadPage() {
                 checked={yeastType === "dry"}
                 onChange={handleYeastTypeChange}
               />
-              &nbsp;Dry Yeast
+              &nbsp;{dryYeastText}
             </label>
             <label className="block mb-1">
               <input
@@ -179,7 +204,7 @@ export default function BreadPage() {
                 checked={yeastType === "fresh"}
                 onChange={handleYeastTypeChange}
               />
-              &nbsp;Fresh Yeast
+              &nbsp;{freshYeastText}
             </label>
             <label className="block mb-1">
               <input
@@ -188,15 +213,23 @@ export default function BreadPage() {
                 checked={yeastType === "sourdough"}
                 onChange={handleYeastTypeChange}
               />
-              &nbsp;Sourdough Starter
+              &nbsp;{sourdoughText}
             </label>
           </div>
-          <button
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mt-2 py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-            onClick={handleReset}
-          >
-            Reset
-          </button>
+          <div className="relative w-full">
+            <button
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mt-2 py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              onClick={handleReset}
+            >
+              {resetText}
+            </button>
+            <button
+              className="bg-transparent hover:bg-gray-100 absolute bottom-0 end-2"
+              onClick={handleLanguageSwitch}
+            >
+              <span className={`fi fis ${language === "english" ? "fi-is" : "fi-gb"} fiCircle`} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
